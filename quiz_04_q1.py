b763b848c668462d67286e53a67379228cfbaaa3
Torin <74333325+hodgetorin@users.noreply.github.com>
2021-01-23 16:22:46 -0700

    Update quiz_04_q1.py

--- quiz_04_q1.py
+++ quiz_04_q1.py
@@ -16,16 +16,8 @@ Pe = np.zeros(5)
 t_actual = np.linspace(0,20,5)
 P_actual = [200,152,118,93,74]
 
-# for i in range(1,6):
-#    dP[i] = dP[i-1]
-
-#plt.plot(t_actual,P_actual)
 plt.xlabel('time, minutes'); plt.ylabel('P, concentration')
 slope, intercept, r_value,p_value,std_err = stats.linregress(t_actual, P_actual)
-
-#tdouble = np.log(2)/np.log(1+slope)*dt
-#print('tdouble = ', tdouble)
-#K = np.log(2)/tdouble
 
 Pe[0] = P_actual[0]
 for i in range(0,5):
